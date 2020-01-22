b82287722d81a7013d50f5f1876872568e5f4c7a
BhavyanshM <bhavyanshmishra@gmail.com>
2020-01-22 16:39:20 -0600

    Add the bezier surface visualizer

--- scripts/bezier_gen.py
+++ scripts/bezier_gen.py
@@ -7,24 +7,31 @@ import matplotlib.pyplot as plt
 fig = plt.figure(figsize=(10,10))
 ax = plt.axes(projection="3d")
 
-x_line = np.linspace(-3, 3, 6)
-y_line = np.linspace(-3, 3, 6)
+x_line = np.linspace(-1, 2, 4)
+y_line = np.linspace(-1, 2, 4)
+X, Y = np.meshgrid(x_line, y_line)
 
-U, V = np.meshgrid(x_line, y_line)
+u_line = np.linspace(-1, 2, 50)
+v_line = np.linspace(-1, 2, 50)
+U, V = np.meshgrid(u_line, v_line)
+
+W = 20 + U**2 + V**2 - 10*np.cos(U) - 10*np.cos(V)
+Z = 20 + X**2 + Y**2 - 10*np.cos(X) - 10*np.cos(Y)
 
 
-print(U,V)
+print("X:",X)
+print("Y:",Y)
+print("Z:",Z)
 
 # Z = function(U,V)
 
-Z = 20 + U**2 + V**2 - 10*np.cos(U) - 10*np.cos(V)
 # Z = X + Y
 
 # ax.plot_wireframe(X, Y, Z, color='green')
 
-ax.plot_surface(U, V, Z, rstride=1, cstride=1,
+ax.plot_surface(U, V, W, rstride=1, cstride=1,
                 cmap='winter', edgecolor='none')
 
-ax.scatter(U, V, Z, color='r', marker='o')
+ax.scatter(X, Y, Z, color='r', marker='o', s=30)
 
 plt.show()
