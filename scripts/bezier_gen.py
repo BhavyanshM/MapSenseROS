d7683f0637d49bd2683d83a48259ae0ee34b06a3
BhavyanshM <bhavyanshmishra@gmail.com>
2020-01-22 10:51:29 -0600

    Add the bezier points mesh

--- scripts/bezier_gen.py
+++ scripts/bezier_gen.py
@@ -4,25 +4,27 @@ from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig = plt.figure()
+fig = plt.figure(figsize=(10,10))
 ax = plt.axes(projection="3d")
 
-x_line = np.linspace(-2, 2, 5)
-y_line = np.linspace(-2, 2, 5)
+x_line = np.linspace(-3, 3, 6)
+y_line = np.linspace(-3, 3, 6)
 
-X, Y = np.meshgrid(x_line, y_line)
+U, V = np.meshgrid(x_line, y_line)
 
 
-print(X,Y)
+print(U,V)
 
-# Z = function(X,Y)
+# Z = function(U,V)
 
-Z = 20 + X**2 + Y**2 - 10*np.cos(2*np.pi*X) - 10*np.cos(2*np.pi*Y)
+Z = 20 + U**2 + V**2 - 10*np.cos(U) - 10*np.cos(V)
 # Z = X + Y
 
 # ax.plot_wireframe(X, Y, Z, color='green')
 
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+ax.plot_surface(U, V, Z, rstride=1, cstride=1,
                 cmap='winter', edgecolor='none')
+
+ax.scatter(U, V, Z, color='r', marker='o')
 
 plt.show()
