627d36debe24f4f8c458862927dea7f12d23db37
Operator Computer <OperatorComputer@unknown>
2021-02-18 14:28:50 -0600

    Improve run sh

--- Docker/run.sh
+++ Docker/run.sh
@@ -3,11 +3,11 @@ docker run -it \
 	--net=host \
 	--env="DISPLAY" \
 	--volume "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-	--volume "$(pwd)"/SharedVolume:/home/robotlab/SharedVolume:rw \
+	--volume "$(pwd)/../.."/SharedVolume:/home/robotlab/SharedVolume:rw \
 	--volume "$(pwd)/..":/home/robotlab/dev/mapsense_ws/src/MapSenseROS:rw \
 	--volume "$HOME"/.config/JetBrains:/home/robotlab/.config/JetBrains:rw \
 	--privileged \
 	--runtime=nvidia \
 	--gpus all \
 	--device /dev/dri:/dev/dri \
-	bmishra/mapsense-nvidia-ros:latest bash
+	ihmcrobotics/mapsense-nvidia-ros:0.3 bash
