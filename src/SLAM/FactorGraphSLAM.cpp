c70733e5d966b4ae7b1296c65e38f3734c73f649
BhavyanshM <bhavyanshmishra@gmail.com>
2021-03-10 01:16:02 -0600

    Add the initial API for factor-graph SLAM using GTSAM

---
+++ src/SLAM/FactorGraphSLAM.cpp
@@ -0,0 +1,39 @@
+#include "FactorGraphSLAM.h"
+
+void FactorGraphSLAM::addPriorPoseFactor(Pose3 mean, Vector3 variance)
+{
+   noiseModel::Diagonal::shared_ptr priorNoise = noiseModel::Diagonal::Sigmas(variance);
+   graph.add(PriorFactor<Pose3>(poseIndex, mean, priorNoise));
+}
+
+void FactorGraphSLAM::addOdometryFactor(Pose3 odometry, Vector3 odomVariance)
+{
+   noiseModel::Diagonal::shared_ptr odometryNoise = noiseModel::Diagonal::Sigmas(odomVariance);
+   graph.add(BetweenFactor<Pose3>(poseIndex, poseIndex + 1, odometry, odometryNoise));
+   poseIndex++;
+}
+
+void FactorGraphSLAM::addOrientedPlaneLandmarkFactor(Vector4 lmMean, Vector3 lmVariance, int lmIndex)
+{
+   noiseModel::Diagonal::shared_ptr lmNoise = noiseModel::Diagonal::Sigmas(lmVariance);
+   graph.add(OrientedPlane3Factor(lmMean, lmNoise, lmIndex, poseIndex));
+}
+
+void FactorGraphSLAM::initPoseValue(int index, Pose3 value){
+   initial.insert(index, value);
+}
+
+void FactorGraphSLAM::initOrientedPlaneLandmarkValue(int index, OrientedPlane3 value){
+   initial.insert(index, value);
+}
+
+void FactorGraphSLAM::optimize()
+{
+   result = LevenbergMarquardtOptimizer(graph, initial).optimize();
+}
+
+Values FactorGraphSLAM::getResults()
+{
+   return result;
+}
+
---
+++ src/SLAM/FactorGraphSLAM.h
@@ -0,0 +1,43 @@
+//
+// Created by quantum on 3/9/21.
+//
+
+#ifndef FACTORGRAPHSLAM_H
+#define FACTORGRAPHSLAM_H
+
+#include <gtsam/slam/OrientedPlane3Factor.h>
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/nonlinear/ISAM2.h>
+#include <gtsam/inference/Symbol.h>
+#include <gtsam/base/numericalDerivative.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+
+#include <CppUnitLite/TestHarness.h>
+#include <boost/bind.hpp>
+#include <boost/assign/std/vector.hpp>
+
+using namespace boost::assign;
+using namespace gtsam;
+using namespace std;
+
+class FactorGraphSLAM
+{
+   private:
+      ISAM2 isam2;
+      Values initial, result;
+      NonlinearFactorGraph graph;
+      int poseIndex = 1;
+
+   public:
+      void addPriorPoseFactor(Pose3 mean, Vector3 variance);
+      void addOdometryFactor(Pose3 odometry, Vector3 odomVariance);
+      void addOrientedPlaneLandmarkFactor(Vector4 lmMean, Vector3 lmVariance, int lmIndex);
+      void optimize();
+      void initPoseValue(int index, Pose3 value);
+      void initOrientedPlaneLandmarkValue(int index, OrientedPlane3 value);
+      Values getResults();
+
+};
+
+#endif //FACTORGRAPHSLAM_H
