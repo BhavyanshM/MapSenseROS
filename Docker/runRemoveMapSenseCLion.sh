a8b2038490aaa9a2f8defe3fc3034e464d84f47c
bhavyansh <bm37@students.uwf.edu>
2021-07-15 17:47:19 -0500

    Remove the JetBrains config mapping in docker on mapsense starter script.

--- Docker/runRemoveMapSenseCLion.sh
+++ Docker/runRemoveMapSenseCLion.sh
@@ -25,7 +25,6 @@ if [[ "$OS_NAME" == *"Arch"* ]]; then
   	--volume "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   	--volume "$(pwd)/../.."/SharedVolume:/home/robotlab/SharedVolume:rw \
   	--volume "$(pwd)/..":/home/robotlab/dev/mapsense_ws/src/MapSenseROS:rw \
-  	--volume "$HOME"/.config/JetBrains:/home/robotlab/.config/JetBrains:rw \
   	--privileged \
   	--gpus all \
   	--device /dev/dri:/dev/dri \
@@ -38,7 +37,6 @@ else
     --volume "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume "$(pwd)/../.."/SharedVolume:/home/robotlab/SharedVolume:rw \
     --volume "$(pwd)/..":/home/robotlab/dev/mapsense_ws/src/MapSenseROS:rw \
-    --volume "$HOME"/.config/JetBrains:/home/robotlab/.config/JetBrains:rw \
     --privileged \
     --runtime=nvidia \
     --gpus all \
